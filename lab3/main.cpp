619a1b23c94f5a6896bc5690dc6fa7ddf61d7874
Ilya Trubachev <trubachev.ii@phystech.edu>
2020-11-15 17:12:48 +0300

    lab3 added

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,46 +1,30 @@
 #include <iostream>
 #include <cmath>
 #include <functional>
+#include <string>
 
-long double solve_with_MPI(std::function<long double(long double)> func);
-long double solve_with_Newton();
+long double solve(std::string name, std::function<long double(long double)> func);
 
 int main()
 {
-    solve_with_MPI([](long double x){return std::sqrt(std::pow(0.5, x) + 1) + 1;});
-    solve_with_MPI([](long double x){return (-1) * std::log2((x - 1) * (x - 1) - 1);});
-    solve_with_MPI([](long double x){return x - (std::pow(0.5, x) + 1 - (x - 1) * (x - 1)) / 2;});
-    solve_with_Newton();
+    solve("MPI", [](long double x){return std::sqrt(std::pow(0.5, x) + 1) + 1;});
+    solve("MPI", [](long double x){return (-1) * std::log2((x - 1) * (x - 1) - 1);});
+    solve("MPI", [](long double x){return x - (std::pow(0.5, x) + 1 - (x - 1) * (x - 1)) / 2;});
+    solve("Newton", [](long double x){return x + (std::pow(0.5, x) + 1 - std::pow(x - 1, 2)) / 
+                                                 (std::log(2) * std::pow(0.5, x) + 2 * (x - 1));});
     return 0;
 }
 
-long double solve_with_MPI(std::function<long double(long double)> func)
+long double solve(std::string name, std::function<long double(long double)> func)
 {
-    long double result = -5.0;
+    long double result = -1.0;
     for (int i = 0; i < 100; ++i)
     {
         long double prev = result;
         result = func(result);
         if (prev == result)
         {
-            std::cout << "MPI is converged towards " << result << " using " << i - 1 << " iterations\n";
-            break;
-        }
-    }
-    return result;
-}
-
-long double solve_with_Newton()
-{
-    long double result = 0.0;
-    for (int i = 0; i < 10; ++i)
-    {
-        long double prev = result;
-        result = result + (std::pow(0.5, result) + 1 - std::pow(result - 1, 2)) / 
-                          (std::log(2) * std::pow(0.5, result) + 2 * (result - 1));
-        if (prev == result)
-        {
-            std::cout << "Newton is converged towards " << result << " using " << i - 1 << " iterations\n";
+            std::cout << name << " is converged towards " << result << " using " << i - 1 << " iterations\n";
             break;
         }
     }
